0c1d952a3f62eec0035d86261deada210c5201e8
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-05-05 18:22:34 +0800

    Add simple logic that runs gson

--- src/main/java/com/myles/demo/App.java
+++ src/main/java/com/myles/demo/App.java
@@ -1,13 +1,15 @@
 package com.myles.demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.google.gson.Gson;
+
+public class App {
+    public static void main( String[] args ){
+        // Serialization
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(1));            // ==> 1
+        System.out.println(gson.toJson("abcd"));       // ==> "abcd"
+        System.out.println(gson.toJson(new Long(10))); // ==> 10
+        int[] values = { 1 };
+        System.out.println(gson.toJson(values));       // ==> [1]
     }
 }
